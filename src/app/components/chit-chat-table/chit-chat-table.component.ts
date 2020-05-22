8666186a286f111d6eeb2ce37e81623e63bd9658
Adam Callanan <a.callanan@sbcglobal.net>
2020-05-22 00:42:34 -0500

    more admin function

--- src/app/components/chit-chat-table/chit-chat-table.component.ts
+++ src/app/components/chit-chat-table/chit-chat-table.component.ts
@@ -1,17 +1,42 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {ChitChat} from "../../models/ChitChat.model";
+import {BooleanHelper} from "../../utilities/boolean.util";
+import {NavHelperService} from "../../services/nav-helper.service";
+import {ChitChatService} from "../../services/chit-chat.service";
 
 @Component({
   selector: 'app-chit-chat-table',
   templateUrl: './chit-chat-table.component.html',
   styleUrls: ['./chit-chat-table.component.scss']
 })
-export class ChitChatTableComponent implements OnInit {
+export class ChitChatTableComponent {
   @Input() public chitChats: ChitChat[] = [];
 
-  constructor() { }
+  public get hasChats(): boolean {
+    return BooleanHelper.hasValue(this.chitChats) && this.chitChats.length > 0;
+  }
 
-  ngOnInit() {
+  constructor(
+    private navHelperService: NavHelperService,
+    private chitChatService: ChitChatService,
+  ) {
+  }
+
+  public edit(chitChat: ChitChat) {
+    this.navHelperService.goToChitChatEditForm(chitChat._id);
+  }
+
+  public delete(chitChat: ChitChat) {
+    let response;
+    this.chitChatService.delete(chitChat._id)
+      .subscribe((res) => response = res,
+        (error) => {
+          console.log("delete chitchat failed");
+        }, () => {
+          this.chitChats = this.chitChats.filter((cc) => {
+            return cc._id !== chitChat._id;
+          });
+        });
   }
 
 }
